e62cd5a433414a0207632eb454c5844cb18cb426
hunglnl01 <hunglnl01@gmail.com>
2020-08-25 03:35:00 -0700

    wokring on the logic

--- monitor.js
+++ monitor.js
@@ -15,7 +15,22 @@ let cart = []
 
 // getting the products
 class Products{
-
+    async getProducts(){
+            try{
+                let result = await fetch('products.json');
+                let data = await result.json();
+                let products = data.items;
+                products = products.map(item =>{
+                    const {title,price} = item.fields;
+                    const {id} = item.sys;
+                    const image = item.fields.image.fields.file.url;
+                    return {title,price,id,image};
+                })
+                return products;
+            } catch (error){
+                console.log(error);
+            }
+    }
 }
 
 // display products
@@ -29,6 +44,9 @@ class Storage{
 }
 
 document.addEventListener("DOMContentLoaded",()=>{
-    const ui = new UI()
-    const products = new Products()
+    const ui = new UI();
+    const products = new Products();
+
+    // get all products
+    products.getProducts().then(data => console.log(data));
 })
